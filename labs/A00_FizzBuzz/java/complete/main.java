39ccd319a6752a8262a768ffb371d3722cf0822a
bathcat <you@example.com>
2021-02-25 10:29:09 -0700

    Adding java implementation.

--- labs/A00_FizzBuzz/java/complete/main.java
+++
@@ -1,7 +0,0 @@
-package A00_FizzBuzz.java.complete;
-
-public class main {
-  public static void main(String[] args){
-    System.out.println("Hello FizzBuzz!"); 
-  }
-}
---
+++ labs/A00_FizzBuzz/java/startingpoint/App.java
@@ -0,0 +1,18 @@
+package A00_FizzBuzz.java.startingpoint;
+
+public class App {
+
+  private static String toMessage(int i){
+    return String.valueOf(i);
+  }
+
+  public static void main(String[] args){
+    System.out.println(toMessage(0));
+    System.out.println(toMessage(1));
+    System.out.println(toMessage(2));
+    System.out.println(toMessage(3));
+    System.out.println(toMessage(4));
+
+    System.out.println("\n\nDone!");
+  }
+}
